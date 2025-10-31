462316f49e27b81b09d9ee3a39940f2fa81966b5
AKadirUzmali <akadiruzmali@gmail.com>
2025-11-01 02:13:25 +0300

    Base Virtual Algorithm Class Re-Designed

--- include/Algorithm/Algorithm.h
+++ include/Algorithm/Algorithm.h
@@ -11,23 +11,46 @@
  */
 
 // Include:
+#include <cstdint>
 #include <string>
-#include <stdexcept>
+
+#include <Flag/Flag.h>
+using namespace core::flag;
 
 // Namespace: Core::VirtualBase
 namespace core::virbase
 {
-    // Enum Class: Algorithm Code
-    enum class e_algorithm : size_t
+    // Namespace: Algorithm
+    namespace algorithm
     {
-        UNKNOWN                         = 0,
-        ERROR,
-        WARNING,
-        SUCCESS,
+        // Enum Class: Algorithm Code
+        enum class e_algorithm : size_t
+        {
+            unknown = 0,
+            error,
+            warning,
+            success,
 
-        ERR_KEY_LENGTH_INVALID          = 1000,
-        ERR_NAME_LENGTH_INVALID
-    };
+            err_key_length_invalid = 1000,
+            err_name_length_invalid,
+            err_flag_notchanged,
+
+            succ_flag_default = 2000,
+            succ_flag_changed
+        };
+
+        // Flag: Algorithm Status
+        inline constexpr flag_t flag_free           { 0 << 0 };
+        inline constexpr flag_t flag_notfree        { 1 << 0 };
+        inline constexpr flag_t flag_valid_name     { 0 << 1 };
+        inline constexpr flag_t flag_valid_key      { 0 << 2 };
+        inline constexpr flag_t flag_err_name       { 1 << 1 };
+        inline constexpr flag_t flag_err_key        { 1 << 2 };
+        inline constexpr flag_t flag_decrypt        { 0 << 3 };
+        inline constexpr flag_t flag_encrypt        { 1 << 3 };
+        inline constexpr flag_t flag_idle           { 0 << 4 };
+        inline constexpr flag_t flag_active         { 1 << 4 };
+    }
 
     // Class: Algorithm
     class Algorithm
@@ -36,6 +59,13 @@ namespace core::virbase
             std::string name;
             std::u32string key;
 
+            Flag flag;
+
+            virtual void setIdle()      noexcept;
+            virtual void setActive()    noexcept;
+            virtual void setEncrypt()   noexcept;
+            virtual void setDecrypt()   noexcept;
+
         public:
             static inline constexpr size_t MIN_NAME_SIZE = 1;
             static inline constexpr size_t MAX_NAME_SIZE = 32;
@@ -43,17 +73,88 @@ namespace core::virbase
             static inline constexpr size_t MIN_KEY_SIZE = 1;
             static inline constexpr size_t MAX_KEY_SIZE = 128;
 
-            explicit Algorithm(const std::string&, const std::u32string&);
-            
-            static bool isValidName(const std::string&) noexcept;
-            virtual const std::string& getName() const noexcept;
-            virtual e_algorithm setName(const std::string&) noexcept;
-            
-            static bool isValidKey(const std::u32string&) noexcept;
-            virtual const std::u32string& getKey() const noexcept;
-            virtual e_algorithm setKey(const std::u32string&) noexcept;
+            explicit Algorithm(const std::string&, const std::u32string&) noexcept;
+            ~Algorithm() noexcept;
 
-            virtual void encrypt(std::u32string&) noexcept {};
-            virtual void decrypt(std::u32string&) noexcept {};            
+            virtual bool hasError()     const noexcept;
+            virtual bool hasName()      const noexcept;
+            virtual bool hasKey()       const noexcept;
+
+            virtual bool isEncrypt()    const noexcept;
+            virtual bool isDecrypt()    const noexcept;
+            virtual bool isIdle()       const noexcept;
+            virtual bool isActive()     const noexcept;
+            
+            virtual const std::string& getName()    const noexcept;
+            virtual const std::u32string& getKey()  const noexcept;
+
+            virtual algorithm::e_algorithm setName(const std::string&)     noexcept;
+            virtual algorithm::e_algorithm setKey(const std::u32string&)   noexcept;
+
+            inline bool encrypt(std::u32string&) noexcept;
+            inline bool decrypt(std::u32string&) noexcept;
+
+            static bool checkValidName(const std::string&)     noexcept;
+            static bool checkValidKey(const std::u32string&)   noexcept;
+
+        protected:
+            virtual bool doEncrypt(std::u32string&) noexcept { return false; };
+            virtual bool doDecrypt(std::u32string&) noexcept { return false; };
     };
+
+    using namespace algorithm;
+
+    /**
+     * @brief [Public] Encrypt
+     * 
+     * Şifreleme yapmayı sağlayacak fonksiyon
+     * ve güvenlik kontrollü olabilmesi için
+     * ana sınıf içinde değiştirilemez olacak
+     * ve güvenlik kısmından sonra asıl şifreleme
+     * çalışması için ayrılmış olan fonksiyon çalışacak
+     * 
+     * @param u32string& Text
+     * @return bool
+     */
+    bool Algorithm::encrypt(std::u32string& _text) noexcept
+    {
+        if( !Algorithm::checkValidName(this->getName()) )
+            this->flag.change(flag_valid_name, flag_err_name);
+
+        if( !Algorithm::checkValidKey(this->getKey()) )
+            this->flag.change(flag_valid_key, flag_err_key);
+
+        this->flag.change(flag_decrypt, flag_encrypt);
+
+        return this->hasError() ?
+            false :
+            this->doEncrypt(_text) && !this->hasError();
+    }
+
+    /**
+     * @brief [Public] Decrypt
+     * 
+     * Şifre çözmeyi sağlayacak fonksiyon
+     * ve güvenlik kontrollü olabilmesi için
+     * ana sınıf içinde değiştirilemez olacak
+     * ve güvenlik kısmından sonra asıl şifre çözücünün
+     * çalışması için ayrılmış olan fonksiyon çalışacak
+     * 
+     * @param u32string& Text
+     * @return bool
+     */
+    bool Algorithm::decrypt(std::u32string& _text) noexcept
+    {
+        if( !Algorithm::checkValidName(this->getName()) )
+            this->flag.change(flag_valid_name, flag_err_name);
+
+        if( !Algorithm::checkValidKey(this->getKey()) )
+            this->flag.change(flag_valid_key, flag_err_key);
+
+        this->flag.change(flag_encrypt, flag_decrypt);
+
+        return this->hasError() ?
+            false :
+            this->doDecrypt(_text) && !this->hasError();
+    }
 }
