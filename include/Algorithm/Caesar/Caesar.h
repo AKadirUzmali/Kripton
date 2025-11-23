20e810b9b46e4268804a7c6159251ce9e7a98c1a
AKadirUzmali <akadiruzmali@gmail.com>
2025-11-24 00:27:11 +0300

    Source file deleted, improvements

--- include/Algorithm/Caesar/Caesar.h
+++ include/Algorithm/Caesar/Caesar.h
@@ -23,8 +23,72 @@ namespace core::crypt::algorithm
 
             explicit Caesar(const std::u32string&);
 
-        protected:
-            virtual bool doEncrypt(std::u32string&) noexcept override;
-            virtual bool doDecrypt(std::u32string&) noexcept override;
+            virtual bool encrypt(std::u32string&) noexcept override;
+            virtual bool decrypt(std::u32string&) noexcept override;
     };
+
+    /**
+     * @brief [Public] Constructor
+     * 
+     * Caesar şifreleme/çözme işlemleri için
+     * gerekli anahtarı alsın ve normal algoritma
+     * sınıf oluşturmasını tamamlasın
+     * 
+     * @param u32string& Key
+     */
+    Caesar::Caesar(const std::u32string& _key)
+    : Algorithm(Caesar::NAME, _key)
+    {}
+
+    /**
+     * @brief [Public] Encrypt
+     * 
+     * Caesar şifreleme de anahtar alınır ve
+     * her turda anahtar ile şifreleme yapılır
+     * 
+     * @param u32string& Text
+     */
+    bool Caesar::encrypt(std::u32string& _text) noexcept
+    {
+        // anahtar bilgisini geçici olarak tutsun
+        const std::u32string& tmp_key = this->getKey();
+        const size_t tmp_key_len = tmp_key.length();
+
+        // geçerli anahtar veya metin uzunluğu yok
+        if( !tmp_key_len || !_text.length() )
+            return false;
+
+        // anahtarı döndürerek kullansın
+        for( size_t counter = 0; counter < _text.length(); ++counter ) {
+            _text.at(counter) += tmp_key.at(counter % tmp_key_len);
+        }
+
+        return true;
+    }
+
+    /**
+     * @brief [Public] Decrypt
+     * 
+     * Caesar şifreleme ile aynı işlemi yapar
+     * fakat bu sefer tam aksine şifre çözer
+     * 
+     * @param u32string& Text
+     */
+    bool Caesar::decrypt(std::u32string& _text) noexcept
+    {
+        // anahtar bilgisini geçici olarak tutsun
+        const std::u32string& tmp_key = this->getKey();
+        const size_t tmp_key_len = tmp_key.length();
+
+        // geçerli anahtar veya metin uzunluğu yok
+        if( !tmp_key_len || !_text.length() )
+            return false;
+
+        // anahtarı döndürerek kullansın
+        for( size_t counter = 0; counter < _text.length(); ++counter ) {
+            _text.at(counter) -= tmp_key.at(counter % tmp_key_len);
+        }
+
+        return true;
+    }
 }
