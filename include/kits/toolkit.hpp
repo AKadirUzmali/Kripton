831969df39309fde59b6141f9fbbb0f6d994168a
AKadirUzmali <akadiruzmali@gmail.com>
2026-01-23 00:12:16 +0300

    Output and Toolkit (Fixed and Patched)

---
+++ include/kits/toolkit.hpp
@@ -0,0 +1,13 @@
+// Abdulkadir U. - 21/01/2026
+#pragma once
+
+// Include
+#include <core/platform.hpp>
+#include <core/flag.hpp>
+#include <core/status.hpp>
+#include <core/version.hpp>
+
+#include <tools/time/time.hpp>
+#include <tools/console/color.hpp>
+#include <tools/hash/hash.hpp>
+#include <tools/charset/utf.hpp>
