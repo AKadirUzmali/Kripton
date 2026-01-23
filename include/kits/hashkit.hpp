dbb972093373dce010690afb5a0ede9a33b78fd6
AKadirUzmali <akadiruzmali@gmail.com>
2026-01-23 21:48:10 +0300

    HashKit for Hashes

---
+++ include/kits/hashkit.hpp
@@ -0,0 +1,12 @@
+// Abdulkadir U. - 21/01/2026
+#pragma once
+
+/**
+ * Hash Kit (Karma Seti)
+ * 
+ * Verileri karmaşıklaştırarak şifrelemeyi sağlayan
+ * temel yapı
+ */
+
+// Include
+#include <tools/hash/vch.hpp>
